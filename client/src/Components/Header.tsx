e028fc0faa1dbfa157a21ae8f895e669bd27bf86
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-03-13 22:03:33 +0200

    complete the header and the footer UI

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,8 +1,14 @@
+import { LuMenu } from "react-icons/lu";
+
 const Header = () => {
   return (
     <header>
       <div className="container">
         <div className="header-top">
+          <div className="header-menu">
+            <LuMenu className="header-menu__icons" />
+            <p className="header-menu__text">Меню</p>
+          </div>
           <nav className="header-nav">
             <ul className="header-nav-list">
               <li className="header-nav-list__element">О компании</li>
@@ -21,21 +27,37 @@ const Header = () => {
             <button className="header-inform__button">заказать звонок</button>
           </div>
         </div>
+
+
+
+
+
+
+
+
+
         <div className="header-bottom">
           <img
             className="header-bottom__logo"
             src="/public/images/image/logo 1.svg "
             alt="logo"
           />
-          <button className=" header-bottom__btn ">
-            <img src="../../public/images/icons/menu.svg" alt="" /> Каталог
-          </button>
-          <form className="header-search-form">
-            <input className="header-search-input" type="text" />
-            <img className="header-search-img" src="../../public/images/icons/search.svg" alt="" />
-          </form>
+          <div className="header-bottom__group">
+            <button className=" header-bottom__btn ">
+              <img src="../../public/images/icons/menu.svg" alt="" /> Каталог
+            </button>
+            <form className="header-search-form">
+              <input className="header-search-input" type="text" />
+              <img
+                className="header-search-img"
+                src="../../public/images/icons/search.svg"
+                alt=""
+              />
+            </form>
+          </div>
+
           <ul className="header-bottom__list">
-            <li className="header-bottom__list-element">
+            <li className="header-bottom__list-element  header-bottom__list-element_invisible">
               <img
                 className="header-bottom__list-img"
                 src="../../public/images/icons/Present.svg"
