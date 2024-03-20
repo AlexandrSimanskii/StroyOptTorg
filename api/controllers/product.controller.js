6933437980c070be8f00628dbcfb77c69abb8369
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-03-20 14:40:45 +0200

    create MongoDB and connect with her

---
+++ api/controllers/product.controller.js
@@ -0,0 +1,7 @@
+export const getProducts = async (req, res) => {
+  try {
+    res.json("ewrfsdx");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-
+import productRouter from "./routes/product.router.js";
 const app = express();
 
 app.listen(3004, () => {
   console.log("Server is running on port 3004");
 });
+ 
+app.use(express.json()); 
+app.use("/api/products", productRouter); 
+  
