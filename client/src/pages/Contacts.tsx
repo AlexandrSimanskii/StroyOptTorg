9d628d5010a6264e7ed7b5a943aa11fc6c1b9fc3
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-04-05 22:40:08 +0200

    creare Ui Contacts page

--- client/src/pages/Contacts.tsx
+++ client/src/pages/Contacts.tsx
@@ -1,9 +1,11 @@
+import { contactsOffice, contactsRegion } from "../utils/data/contactData";
+
 const Contacts = () => {
   return (
     <div className="contacts">
       <div className="container">
-        <h2>Контакты </h2>
-        <div className="contacts-location">
+        <h2> Контакты </h2>
+        <section className="contacts-location">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36803.90749846217!2d20.849255434399375!3d54.793251250892084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e3728cece1f9bd%3A0xa021222f874ada0!2z0JfQsNGA0LXRh9GM0LUsINCa0LDQu9C40L3QuNC90LPRgNCw0LTRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8sIDIzODMyMw!5e0!3m2!1sru!2suk!4v1712174007417!5m2!1sru!2suk"
             loading="lazy"
@@ -11,7 +13,7 @@ const Contacts = () => {
 
           <dl className="location-groups">
             <div className="location-group location-group__location">
-              <dt className="location-group__name ">Адрес:</dt>
+              <dt className="location-group__name "> Адрес:</dt>
               <dd className="location-group__description">
                 369012, Карачаево-Черкесская Республика, г. Черкесск, ул
                 Октябрьская, дом 301
@@ -36,7 +38,64 @@ const Contacts = () => {
             </div>
             <button className="location-btn ">Заказать звонок </button>
           </dl>
-        </div>
+        </section>
+        <section className="office">
+          <div className="office__cards">
+            {Object.entries(contactsOffice).map(([key, value]) => (
+              <div className="office__card" key={key}>
+                <p>{key}</p>
+                <p className="contacts-numbers">{value}</p>
+              </div>
+            ))}
+          </div>
+          <div className="office__props">
+            <span> Реквизиты: </span>
+            <br />
+            ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "СТРОЙОПТТОРГ"ИНН
+            0901051787КПП 090101001369000, Карачаево-Черкесская республика,
+            город Черкесск, Октябрьская улица, 301р/с 40702810360000102415 в
+            Ставропольское отделение №5230 ПАО Сбербанк, БИК 040702615
+          </div>
+        </section>
+        <section className="regions">
+          <h2>Работаем по регионам:</h2>
+          <div className="regions-cards">
+            {Object.entries(contactsRegion).map(([key, value]) => (
+              <div key={key} className="regions-card">
+                <p>{key}</p>
+                <p className="contacts-numbers">{value}</p>
+                <a href="info@stroiopttorg.ru">info@stroiopttorg.ru</a>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="contact-form">
+          <h3>У вас есть вопросы? С радостью ответим на них!</h3>
+          <form>
+            <div className="contact-box">
+              <label className="contact-label">
+                Ваше имя <span>*</span>:
+                <input type="text" placeholder="Введите ваше имя" />
+              </label>{" "}
+              <label className="contact-label">
+                Номер телефона <span>*</span>:{" "}
+                <input type="text" placeholder="+7 (     )     -       -" />
+              </label>
+            </div>
+            <label className="contact-label">
+              Текст сообщения <span>*</span>:{" "}
+              <textarea placeholder="Введите ваш вопрос"></textarea>
+            </label>{" "}
+            <div className="contact-box">
+              <button className="contact-form__btn">отправить </button>
+
+              <label className="contact-checkbox-group">
+                <input type="checkbox" /> Согласен с обработкой персональных
+                данных в соответствии с политикой конфиденциальности
+              </label>
+            </div>
+          </form>
+        </section>
       </div>
     </div>
   );
